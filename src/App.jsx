a25438956fdf639fd19551db00038e0df6e16736
NadielySilva <nady.ceng@gmail.com>
2022-06-15 18:52:21 -0300

    nome do e-commerce added

--- src/App.jsx
+++ src/App.jsx
@@ -353,6 +353,7 @@ export default function App() {
 
   return (
     <div>
+      <h2>e-commerce T2</h2>
       <input value={input} 
       onChange={(e)=>{setInput(e.target.value)}}/>
       <button onClick={()=>{handleClick()}}>Enviar</button>
